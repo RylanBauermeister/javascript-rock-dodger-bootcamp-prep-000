263f71c12aad7e7beb91935f797d55cd219576a8
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-06 00:22:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -128,8 +128,7 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-   var leftNumber = DODGER.style.left.replace('px', '');
-   var left = parseInt(leftNumber);
+   var left = positionToInteger(DODGER.style.left);
    if(left < 360){
       DODGER.style.left = `${left + 4}px`;
    }
