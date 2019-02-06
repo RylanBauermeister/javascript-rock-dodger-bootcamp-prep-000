7e3aa796a4bdf82e6acccd085af2e74109b324aa
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-06 00:29:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ function createRock(x) {
      * If a rock collides with the DODGER,
      * we should call endGame()
      */
-    top -= 2;
+    top += 2;
     rock.style.top = `${top}px`
 
     /**
