376c504e3166a5055f15e3150eadcccf9e6c8488
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-06 01:14:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -87,10 +87,11 @@ function endGame() {
 }
 
 function moveDodger(e) {
-   e.preventDefault();
    if(e.which === LEFT_ARROW){
+     e.preventDefault();
      moveDodgerLeft();
    } else if(e.which === RIGHT_ARROW){
+     e.preventDefault();
      moveDodgerRight();
    }
 }
